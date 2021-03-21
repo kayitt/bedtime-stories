9e7f5ed63dda47bdd571f50f3c8c5b64eeed77fe
Pawel Polanski <pawel@upday.com>
2021-03-21 22:15:54 +0100

    Cleanup

--- webhooks/ActionsOnGoogleFulfillment/index.js
+++ webhooks/ActionsOnGoogleFulfillment/index.js
@@ -34,10 +34,15 @@ app.handle('wake_up_time', async (conv) => {
 app.handle('whole_story', async (conv) => {
   const allData = await currentData();
 
+  const maxTemp = allData.temperature_outside.max;
+  const minTemp = allData.temperature_outside.min;
+
   conv.add(
   `Today you have woken up at a ${timeUtils.toSimpleTime(allData.wake_up_time.toDate())}. 
   Since then you have made ${allData.num_tea_boils} cups of tea.
-  Current temperature at home is ${allData.temperature_inside.current} degrees celsius.`
+  Current temperature at home is ${allData.temperature_inside.current} degrees celsius.
+  The coldest it has been outside was ${minTemp.value} degrees at ${timeUtils.toSimpleTime(minTemp.ts.toDate())} 
+  and the warmest ${maxTemp.value} degrees at ${timeUtils.toSimpleTime(maxTemp.ts.toDate())}.`
   );
 });
 
