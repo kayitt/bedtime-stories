1a59e1b02686951de42b3014eb8cdce84ccd078b
Helena Peña <36867202+kayitt@users.noreply.github.com>
2021-04-05 18:49:38 +0200

    WIP

---
+++ etl/test/test_extractor.py
@@ -0,0 +1,33 @@
+from unittest import TestCase
+
+from etl.src.extractor import Extractor
+import pandas as pd
+from pandas.testing import assert_series_equal
+
+
+class TestExtractor(TestCase):
+
+    def test_accepts_connection(self):
+        Extractor(connection="connection")
+
+    def test_has_extract_accepts_query(self):
+        Extractor(connection="connection").extract(query="query")
+
+    def test_returns_time_series(self):
+        index = pd.to_datetime([1615746795223, 1615747408794], unit="ms")
+        data = pd.Series([502, 535], index=index)
+
+        extracted_data = Extractor(connection="connection").extract(query="query")
+
+        assert_series_equal(data, extracted_data)
+
+    def test_returns_different_data(self):
+        index = pd.to_datetime([1615746795223, 1615747408794], unit="ms")
+        data = pd.Series([901, 865], index=index)
+
+        extracted_data = Extractor(connection="connection").extract(query="another_query")
+
+        assert_series_equal(data, extracted_data)
+
+
+
