702d926076180e0027fce585806639e3f62477b5
Helena Peña <thaki1110@gmail.com>
2021-04-05 21:28:31 +0200

    WIP

--- etl/src/extractor.py
+++ etl/src/extractor.py
@@ -5,17 +5,21 @@ import json
 
 class HomeAPI:
     def __init__(self):
-        self.url = "http://user:user@192.168.178.94:8080/api/hassio_ingress" \
-                   "/uRmXKLfm0vxpbj_90ivhuGFWRFbJb3bVcVO91AaAwwA/api/datasources/proxy/1/query"
+        self.url = (
+            "http://user:user@192.168.178.94:8080/api/hassio_ingress"
+            "/uRmXKLfm0vxpbj_90ivhuGFWRFbJb3bVcVO91AaAwwA/api/datasources/proxy/1/query"
+        )
         self.params = {"db": "homeassistant", "epoch": "ms"}
 
     def query(self, query):
         self.params["q"] = query
         headers = {
-            'Content-Type': 'application/json',
-            'Authorization': 'Basic dXNlcjp1c2Vy'
+            "Content-Type": "application/json",
+            "Authorization": "Basic dXNlcjp1c2Vy",
         }
-        response = requests.request("GET", self.url, headers=headers, params = self.params)
+        response = requests.request(
+            "GET", self.url, headers=headers, params=self.params
+        )
         return json.loads(response.content)
 
 
@@ -25,8 +29,7 @@ class Extractor:
 
     def extract(self, query):
         result_dict = self.home_api.query(query=query)
-        data = result_dict['results'][0]['series'][0]['values']
+        data = result_dict["results"][0]["series"][0]["values"]
         time = [x[0] for x in data]
         value = [x[1] for x in data]
-        print(time)
         return pd.Series(value, index=pd.to_datetime(time, unit="ms"))
