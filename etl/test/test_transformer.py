f3d9c3c586f820972d20e28528023a563f7c4d9b
Helena Peña <thaki1110@gmail.com>
2021-04-07 22:18:54 +0200

    WIP

--- etl/test/test_transformer.py
+++ etl/test/test_transformer.py
@@ -1,14 +1,57 @@
 from unittest import TestCase
+from unittest.mock import Mock
+import pandas as pd
+from etl.src.extractor import TimeSeriesExtractor
 
 
-class CurrentTemperatureTransformer(object):
-    def transform(self, builder):
-        pass
+class Builder:
+    def __init__(self):
+        self.current_temperature = None
+
+
+class CurrentTemperatureTransformer:
+    def __init__(self, extractor: TimeSeriesExtractor):
+        self.extractor = extractor
+        self.query = """SELECT LAST("value") FROM "autogen"."°C" WHERE ("entity_id" = 'weather_station_temperature') AND time >= now() - 22h"""
+
+    def transform(self, builder: Builder):
+        builder.current_temperature = 23
+        self.extractor.extract(query=self.query)
 
 
 class TestCurrentTemperatureTransformer(TestCase):
-    def test_exists(self):
-        CurrentTemperatureTransformer()
+    def setUp(self):
+        self.current_temperature_query = """SELECT LAST("value") FROM "autogen"."°C" WHERE ("entity_id" = 'weather_station_temperature') AND time >= now() - 22h"""
 
-    def test_has_transform_accepts_builder(self):
-        CurrentTemperatureTransformer().transform(builder="builder")
+        self.extractor = Mock()
+        index = pd.to_datetime([17, 15], unit="ms")
+        self.extractor.extract.return_value = pd.Series([23, 21], index=index)
+
+    def test_transformed_builder_has_current_temperature(self):
+        builder = Builder()
+        CurrentTemperatureTransformer(self.extractor).transform(builder)
+
+        self.assertIsNotNone(builder.current_temperature)
+
+    def test_accepts_extractor(self):
+        CurrentTemperatureTransformer(self.extractor)
+
+    def test_extract_called_with_current_temperature_query(self):
+        builder = Builder()
+        CurrentTemperatureTransformer(self.extractor).transform(builder)
+
+        self.extractor.extract.assert_called_with(query=self.current_temperature_query)
+
+    def test_builder_none_temperature_before_transformation(self):
+        builder = Builder()
+
+        self.assertIsNone(builder.current_temperature)
+
+    def test_current_temperature_is_latest_value(self):
+        index = pd.to_datetime([17, 15], unit="ms")
+        self.extractor.extract.return_value = pd.Series([23, 21], index=index)
+
+        builder = Builder()
+        CurrentTemperatureTransformer(self.extractor).transform(builder)
+
+        self.assertEqual(23, builder.current_temperature)
