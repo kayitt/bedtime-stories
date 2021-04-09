95d2e621bd519f04f343445d3d2ed780f0b947f1
Paweł Polański <pawel@upday.com>
2021-04-10 00:30:40 +0200

    Create better logs

--- update.sh
+++ update.sh
@@ -2,6 +2,17 @@
 
 set -e
 
+# echo 'Upgrade pip'
+
+# python3.9 -m pip install --upgrade pip
+
+# echo 'Install dependencies'
+
+# SCRIPT=`realpath $0`
+# SCRIPTPATH=`dirname $SCRIPT`
+
+# pip install -r $SCRIPTPATH/etl/requirements.txt
+
 echo 'Run ETL...'
 
 python3.9 etl/src/main.py
