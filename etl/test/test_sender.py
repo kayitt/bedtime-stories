3dc3693583c48bbdb2f01ca2aabb948bb3196ffd
Helena Peña <thaki1110@gmail.com>
2021-04-09 22:07:50 +0200

    WIP

--- etl/test/test_sender.py
+++ etl/test/test_sender.py
@@ -1,30 +1,7 @@
 from unittest import TestCase
 from unittest.mock import Mock
-
 from etl.src.data_classes import Model
-from google.cloud.firestore_v1.client import Client
-
-
-class FirestoreSender:
-    def __init__(self, client: Client):
-        self.client = client
-
-    def send(self):
-        pass
-
-
-class Sender:
-    def __init__(self, sender: FirestoreSender):
-        self.sender = sender
-
-    def send(self, model: Model):
-        self.sender.send()
-
-
-class UnableToLoadException(Exception):
-    """Raised when upload to Firestore fails"""
-
-    pass
+from etl.src.sender import Sender, UnableToLoadException
 
 
 class TestSender(TestCase):
@@ -39,3 +16,12 @@ class TestSender(TestCase):
 
         with self.assertRaises(UnableToLoadException):
             Sender(self.firestore_sender).send(model=Model(current_temperature=21))
+
+    def test_sender_sends_model_dictionary(self):
+        self.firestore_sender = Mock()
+
+        Sender(self.firestore_sender).send(model=Model(current_temperature=21))
+
+        self.firestore_sender.send.assert_called_with(
+            {"temperature_inside": {"current": 21}}
+        )
