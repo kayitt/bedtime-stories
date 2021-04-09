f30ef48ccac415df2631b48e50ee0d9d136c0bec
Helena Peña <thaki1110@gmail.com>
2021-04-10 00:17:54 +0200

    WIP

--- etl/src/main.py
+++ etl/src/main.py
@@ -2,7 +2,13 @@ import sys
 
 sys.path.append(".")
 
-from etl.src.transformer import Director, CurrentTemperatureTransformer
+from etl.src.transformer import (
+    Director,
+    CurrentTemperatureTransformer,
+    TeaBoilsTransformer,
+    WakeUpTimeTransformer,
+    CompositeTransformer,
+)
 from etl.src.extractor import TimeSeriesExtractor, HomeAPI
 from etl.src.sender import Sender, FirestoreSender
 from google.cloud import firestore
@@ -11,8 +17,18 @@ from google.cloud import firestore
 def report():
     client = firestore.Client()
 
-    director = Director(CurrentTemperatureTransformer(TimeSeriesExtractor(HomeAPI())))
+    extractor = TimeSeriesExtractor(HomeAPI())
+
+    current_temp = CurrentTemperatureTransformer(extractor)
+    num_tea_boils = TeaBoilsTransformer(extractor)
+    wake_up_time = WakeUpTimeTransformer(extractor)
+
+    composite = CompositeTransformer([current_temp, num_tea_boils, wake_up_time])
+
+    director = Director(composite)
+
     firestore_sender = FirestoreSender(client)
+
     Sender(firestore_sender).send(director.create_report())
 
 
